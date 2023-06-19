419f24f83a654206ebbc9af33c21b4446d49e9d4
dabucz <76279444+dabucz@users.noreply.github.com>
2023-06-19 17:08:45 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+API_ENDPOINT = "https://discord.com/api/v8"
+REDIRECT_URI = "http://localhost:8000/auth/callback"
+BOT_TOKEN = ""
+GUILD_IDS = [] # must be integer AND bot must be on the guilds
+HOST = "0.0.0.0"
+PORT = 8000
